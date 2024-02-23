e1cee2a73d596e632d4e5a94248a654e0fc8d3af
AJTAWAN <65011212230@msu.ac.th>
2024-02-23 12:25:05 +0700

    add angular

---
+++ pain/src/app/votemain/votemain.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-votemain',
+  standalone: true,
+  imports: [],
+  templateUrl: './votemain.component.html',
+  styleUrl: './votemain.component.scss'
+})
+export class VotemainComponent {
+
+}
